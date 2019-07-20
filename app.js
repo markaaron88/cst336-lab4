1e1dc837e6eecb8abb3536780dfbdd2ff9443d4c
mamariscal <mamariscal@csumb.edu>
2019-07-20 12:55:20 -0400

    fixed errors

--- app.js
+++ app.js
@@ -4,16 +4,16 @@ app.engine('html', require('ejs').renderFile);
 app.use(express.static("public"));
 
 app.get("/", function(req,res){
-  res.send("index.html");
+  res.render("index.html");
 });
 
 app.get("/mercury", function(req, res){
-  res.send("This will be Mercury web page!");
-})
+  res.render("mercury.html");
+});
 
 app.get("/venus", function(req, res){
-  res.send("This will be Venus web page!");
-})
+  res.render("venus.html");
+});
 
 //server listener
 app.listen(process.env.PORT, process.env.IP, function(){
