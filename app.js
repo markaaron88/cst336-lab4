8e1892bb3c6ceeffac8e15c95379e9dcbaaff070
mamariscal <mamariscal@csumb.edu>
2019-07-20 21:23:48 -0400

    fix

--- app.js
+++ app.js
@@ -15,6 +15,22 @@ app.get("/venus", function(req, res){
   res.render("venus.html");
 });
 
+app.get("/earth", function(req, res){
+  res.render("earth.html");
+});
+
+app.get("/jupiter", function(req, res){
+  res.render("jupiter.html");
+});
+
+app.get("/earth", function(req, res){
+  res.render("earth.html");
+});
+
+
+
+
+
 //server listener
 app.listen(process.env.PORT, process.env.IP, function(){
    console.log("Express Server is Running...")
